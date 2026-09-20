be1b8ce30f8856839354daf851be4bdc986be9ab
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-09-20 18:08:10 +0530

    Create load-balancer.tf

---
+++ modules/alb/load-balancer.tf
@@ -0,0 +1,20 @@
+resource "aws_lb" "this" {
+  name = "${var.project_name}-${var.environment}-alb"
+
+  internal = false
+
+  load_balancer_type = "application"
+
+  security_groups = [
+    var.security_group_id
+  ]
+
+  subnets = var.public_subnet_ids
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-alb"
+    Environment = var.environment
+  }
+}
