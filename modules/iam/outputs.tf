dbc045e247da8767a1ebe67d54ef9f0eac27817d
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-09-20 18:07:38 +0530

    Add IAM outputs for EC2 role and instance profile

---
+++ modules/iam/outputs.tf
@@ -0,0 +1,11 @@
+output "ec2_role_name" {
+  value = aws_iam_role.ec2.name
+}
+
+output "ec2_role_arn" {
+  value = aws_iam_role.ec2.arn
+}
+
+output "instance_profile_name" {
+  value = aws_iam_instance_profile.ec2.name
+}
