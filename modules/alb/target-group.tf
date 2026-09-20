f6864b98dba60992a15e2107aededc24c6c7093f
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-09-20 18:08:34 +0530

    Add AWS LB target group resource configuration

---
+++ modules/alb/target-group.tf
@@ -0,0 +1,34 @@
+resource "aws_lb_target_group" "app" {
+  name = "${var.project_name}-${var.environment}-tg"
+
+  port = var.target_port
+
+  protocol = "HTTP"
+
+  vpc_id = var.vpc_id
+
+  target_type = "instance"
+
+  health_check {
+    enabled = true
+
+    path = "/health"
+
+    protocol = "HTTP"
+
+    matcher = "200"
+
+    interval = 30
+
+    timeout = 5
+
+    healthy_threshold = 2
+
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-target-group"
+    Environment = var.environment
+  }
+}
