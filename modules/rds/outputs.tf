17082ac1e6503b9f442fdd5175dfff985ba0c843
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-09-20 18:20:17 +0530

    Add outputs for RDS instance details

---
+++ modules/rds/outputs.tf
@@ -0,0 +1,15 @@
+output "db_instance_id" {
+  value = aws_db_instance.mysql.id
+}
+
+output "db_endpoint" {
+  value = aws_db_instance.mysql.address
+}
+
+output "db_port" {
+  value = aws_db_instance.mysql.port
+}
+
+output "db_name" {
+  value = aws_db_instance.mysql.db_name
+}
