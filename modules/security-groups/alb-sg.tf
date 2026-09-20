a75dc899b0ecb38e21f3d46df0c7e470167831db
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-09-20 17:56:13 +0530

    Create alb-sg.tf

---
+++ modules/security-groups/alb-sg.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "alb" {
+  name = "${var.project_name}-${var.environment}-alb-sg"
+
+  description = "Security group for Application Load Balancer"
+
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "HTTP from Internet"
+
+    from_port = 80
+    to_port   = 80
+
+    protocol = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  ingress {
+    description = "HTTPS from Internet"
+
+    from_port = 443
+    to_port   = 443
+
+    protocol = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+
+    from_port = 0
+    to_port   = 0
+
+    protocol = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-alb-sg"
+    Environment = var.environment
+  }
+}
