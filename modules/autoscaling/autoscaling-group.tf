3b226bf210f1fb5318032e4f30d683004fe7ba15
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-09-20 18:12:02 +0530

    Add AWS Autoscaling Group resource configuration

---
+++ modules/autoscaling/autoscaling-group.tf
@@ -0,0 +1,49 @@
+resource "aws_autoscaling_group" "app" {
+  name = "${var.project_name}-${var.environment}-asg"
+
+  min_size = var.min_size
+
+  max_size = var.max_size
+
+  desired_capacity = var.desired_capacity
+
+  vpc_zone_identifier = var.private_app_subnet_ids
+
+  target_group_arns = [
+    var.target_group_arn
+  ]
+
+  health_check_type = "ELB"
+
+  health_check_grace_period = 300
+
+  launch_template {
+    id = var.launch_template_id
+
+    version = var.launch_template_version
+  }
+
+  tag {
+    key = "Name"
+
+    value = "${var.project_name}-${var.environment}-app"
+
+    propagate_at_launch = true
+  }
+
+  tag {
+    key = "Environment"
+
+    value = var.environment
+
+    propagate_at_launch = true
+  }
+
+  tag {
+    key = "Project"
+
+    value = var.project_name
+
+    propagate_at_launch = true
+  }
+}
