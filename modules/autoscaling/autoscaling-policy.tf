5623d3f94da4f91cb4934f594cb683b00713f34a
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-09-20 18:12:25 +0530

    Add autoscaling policy for CPU utilization

---
+++ modules/autoscaling/autoscaling-policy.tf
@@ -0,0 +1,15 @@
+resource "aws_autoscaling_policy" "cpu" {
+  name = "${var.project_name}-${var.environment}-cpu-policy"
+
+  autoscaling_group_name = aws_autoscaling_group.app.name
+
+  policy_type = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 60
+  }
+}
