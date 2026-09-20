9a96b582e4318a630fa98dee4785e8942dc4423d
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-09-20 14:49:42 +0530

    Add public subnets resource to VPC module

---
+++ modules/vpc/public-subnets.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id = aws_vpc.this.id
+
+  cidr_block = var.public_subnet_cidrs[count.index]
+
+  availability_zone = var.availability_zones[count.index]
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-public-${count.index + 1}"
+
+    Environment = var.environment
+
+    Tier = "Public"
+  }
+}
