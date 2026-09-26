2eda7bceee054dfddb510eede6aba204777aeec6
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-09-26 13:46:09 +0530

    Update backend.tf

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "YOUR-TERRAFORM-STATE-BUCKET"
+    bucket       = "sujay-terraform-state-2026"
     key          = "ecommerce/prod/terraform.tfstate"
     region       = "eu-north-1"
     encrypt      = true
